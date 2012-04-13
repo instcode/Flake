ca243014548cb4fc98ddacf0f3861eb34232f23e
Khoa Nguyen <instcode@gmail.com>
2012-04-13 20:31:58 +0700

    Employ redis-py Connection Pool to the benchmark

--- README.md
+++ README.md
@@ -22,31 +22,5 @@ Thrift:
 
 ### Speed
 
-Flake is *really* fast with Thrift protocol.
-Benchmarking agaist a Xeon 2.5Ghz from another machine on the same network:
-
-    python benchmark.py -h xeon-server:8000 -n 10000 -c 50
-
-Result:
-
-    Concurrency Level:      50
-    Time taken for tests:   0.200 seconds
-    Complete requests:      10000
-    Failed requests:        0 (0.00%)
-    Requests per second:    50067.85[#/sec] (mean)
-    Time per request:       0.0000 [ms] (average)
-    Total transferred:      0 bytes
-    Transfer rate:  0 [Kbytes/sec]
-    
-    Connection Times (ms)
-            min     mean    max
-            0.01    0.02    10.75
-    
-    Percentage of the requests served within a certain time (ms)
-            50%     0.013
-            60%     0.014
-            70%     0.015
-            80%     0.019
-            90%     0.037
-            99%     0.066
+Flake is quite fast with Thrift protocol. You may want to run the benchmark.py to measure the performance of Flake.
 
--- benchmark.py
+++ benchmark.py
@@ -2,9 +2,9 @@
 import sys, os
 
 from time import time
+from math import fsum
 import socket
 
-from threading import Thread
 from multiprocessing import Pool
 
 from vng.gpi.flake import Flake
@@ -15,53 +15,56 @@ from thrift.transport import TTransport
 from thrift.transport import TSocket
 from thrift.protocol import TBinaryProtocol
 
-def get_stats():
-    transport = TSocket.TSocket(host, port)
-    transport = TTransport.TBufferedTransport(transport)
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-    client = Flake.Client(protocol)
-    transport.open()
- 
-    stats = client.get_stats()
-    #print "From server: %s" % (stats)
-  
-    transport.close()
+class Connection(object):
 
-def get_timestamp():
-    transport = TSocket.TSocket(host, port)
-    transport = TTransport.TBufferedTransport(transport)
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-    client = Flake.Client(protocol)
-    transport.open()
- 
-    timestamp = client.get_timestamp()
-    #print "From server: %d" % (timestamp)
-  
-    transport.close()
+    def __init__(self, host, port):
+        transport = TSocket.TSocket(host, port)
+        transport = TTransport.TBufferedTransport(transport)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        self.client = Flake.Client(protocol)
+        transport.open()
 
-def get_worker_id():
-    transport = TSocket.TSocket(host, port)
-    transport = TTransport.TBufferedTransport(transport)
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-    client = Flake.Client(protocol)
-    transport.open()
+    def disconnect(self):
+        pass
 
-    id = client.get_worker_id()
-    #print "From server: %d" % (id)
+# TODO: add ability to block waiting on a connection to be released
+class ConnectionPool(object):
+    "Generic connection pool"
+    def __init__(self, connection_class=Connection, max_connections=None,
+                 **connection_kwargs):
+        self.connection_class = connection_class
+        self.connection_kwargs = connection_kwargs
+        self.max_connections = max_connections or 2**31
+        self._created_connections = 0
+        self._available_connections = []
+        self._in_use_connections = set()
 
-    transport.close()
+    def get_connection(self):
+        "Get a connection from the pool"
+        try:
+            connection = self._available_connections.pop()
+        except IndexError:
+            connection = self.make_connection()
+        self._in_use_connections.add(connection)
+        return connection
 
-def get_id():
-    transport = TSocket.TSocket(host, port)
-    transport = TTransport.TBufferedTransport(transport)
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-    client = Flake.Client(protocol)
-    transport.open()
- 
-    id = client.get_id()
-    #print "From server: %d" % (id)
-  
-    transport.close()
+    def make_connection(self):
+        "Create a new connection"
+        if self._created_connections >= self.max_connections:
+            raise ConnectionError("Too many connections")
+        self._created_connections += 1
+        return self.connection_class(**self.connection_kwargs)
+
+    def release(self, connection):
+        "Releases the connection back to the pool"
+        self._in_use_connections.remove(connection)
+        self._available_connections.append(connection)
+
+    def disconnect(self):
+        "Disconnects all connections in the pool"
+        all_conns = chain(self._available_connections, self._in_use_connections)
+        for connection in all_conns:
+            connection.disconnect()
 
 if len(sys.argv) <= 2 or sys.argv[1] == '--help' or '-h' not in sys.argv:
     print ''
@@ -88,40 +91,67 @@ while argi < len(sys.argv) - 1:
         num_reqs = int(sys.argv[argi + 1])
         argi += 2
 
+conn_pool = ConnectionPool(host=host, port=port)
+
+def get_stats():
+    connection = conn_pool.get_connection()
+    client = connection.client
+    stats = client.get_stats()
+    #print "From server: %s" % (stats)
+    conn_pool.release(connection)
+
+def get_timestamp():
+    connection = conn_pool.get_connection()
+    client = connection.client
+    timestamp = client.get_timestamp()
+    conn_pool.release(connection)
+
+def get_worker_id():
+    connection = conn_pool.get_connection()
+    client = connection.client
+    woker_id = client.get_worker_id()
+    conn_pool.release(connection)
+
+def get_id():
+    start = time()
+    try:
+        connection = conn_pool.get_connection()
+        client = connection.client
+        id = client.get_id()
+        conn_pool.release(connection)
+    except Exception, e:
+        error += 1
+    duration = time() - start
+    return duration
+
 pool = Pool(processes=num_conns)
+durations = []
+error = 0
 
 print 'Benchmarking... Plz wait...'
+def add_duration(duration):
+    durations.append(duration)
 
-stats = {'error': 0, 'duration': []}
+begin = time()
 for i in range(num_reqs):
-    start = time()
-    
-    try:
-        pool.apply_async(get_id)
-        #pool.apply_async(get_worker_id)
-        #pool.apply_async(get_timestamp)
-        #pool.apply_async(get_stats)
-    except Exception, e:
-        stats['error'] += 1
-        pass
-    duration = time() - start
-    stats['duration'].append(duration)
+    #pool.apply_async(hell)
+    pool.apply_async(get_id, callback = add_duration)
+    #pool.apply_async(get_worker_id)
+    #pool.apply_async(get_timestamp)
+    #pool.apply_async(get_stats)
 
 pool.close()
 pool.join()
 
-# Compute and print stats
-error = stats['error']
-error_per = error * 100 / num_reqs
-durations = stats['duration']
-durations.sort(reverse=True)
+total_time = time() - begin;
 
-duration = sum(durations)
-complete_reqs = num_reqs - stats['error']
-req_per_sec = complete_reqs / duration
+error_per = error * 100 / num_reqs
+durations.sort(reverse=True)
+duration = fsum(durations)
+complete_reqs = num_reqs - error
+req_per_sec = num_reqs / total_time 
 time_per_req = duration / complete_reqs
 transfer_rate = 0
-throughput = num_conns * num_reqs / duration
 avg_time = 1000 * duration / num_reqs if num_reqs else 0
 min_time = 1000 * durations[-1] if len(durations) else 0
 max_time = 1000 * durations[0] if len(durations) else 0
@@ -134,13 +164,13 @@ ninetynine_p = 1000 * durations[num_reqs / 100] if len(durations) else 0
 
 print ''
 print 'Concurrency Level:\t%d' % (num_conns)
-print 'Time taken for tests:\t%4.3f seconds' % (duration)
+print 'Time taken for tests:\t%.3f secs' % (duration)
 print 'Complete requests:\t', complete_reqs
 print 'Failed requests:\t%d (%1.2f%%)' % (error, error_per)
-print 'Requests per second:\t%.2f[#/sec] (mean)' % (req_per_sec)
+print 'Requests per second:\t%.2f [#/sec] (average)' % (req_per_sec)
 print 'Time per request:\t%.4f [ms] (average)' % (time_per_req)
-print 'Total transferred:\t', 0, 'bytes'
-print 'Transfer rate:\t', transfer_rate, '[Kbytes/sec]'
+#print 'Total transferred:\t', 0, 'bytes'
+#print 'Transfer rate:\t', transfer_rate, '[Kbytes/sec]'
 print ''
 print 'Connection Times (ms)'
 print '\tmin\tmean\tmax'
@@ -159,3 +189,4 @@ print '\t70%%\t%.3f' % (seventy_p)
 print '\t80%%\t%.3f' % (eighty_p)
 print '\t90%%\t%.3f' % (ninety_p)
 print '\t99%%\t%.3f' % (ninetynine_p)
+
